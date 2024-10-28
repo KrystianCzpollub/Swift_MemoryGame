10123a9e9327242f41eb558b67abbf244991988a
KrystianCzpollub <czkrystian04@gmail.com>
2024-10-28 23:51:19 +0100

    Add files via upload

---
+++ MemoryGame/CardView.swift
@@ -0,0 +1,58 @@
+//
+//  CardView.swift
+//  MemoryGame
+//
+//  Created by student on 22/10/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    @State var isReverse: Bool = false
+    var emoji: String
+    var body: some View {
+        ZStack{
+            Group{
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.blue, lineWidth: 6)
+                if isReverse {
+                    Text(emoji)
+                        .font(.largeTitle)
+                        .opacity(isReverse ? 1 : 0)
+                } else {
+                    Text("")
+                }
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(Color.blue)
+                    .opacity(isReverse ? 0 : 1)
+
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isReverse.toggle()
+            }
+        }
+        .frame(width: 100, height: 100)
+        .padding(.top, 10)
+
+        // Group {
+        //     RoundedRectangle(cornerRadius: 12)
+        //         .stroke(lineWidth: 6) //Color.blue, 
+        //         .background(isReverse ? Color.white : Color.blue)
+        //         .cornerRadius(12)
+        //         .overlay(
+        //             Group {
+        //                 if isReverse {
+        //                     Text(emoji)
+        //                         .font(.largeTitle)
+        //                 }
+        //             }
+        //         )
+        // }
+    }
+}
+
+// #Preview {
+//     CardView(emoji: "👻")
+// }
